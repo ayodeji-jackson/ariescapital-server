4996bf4e34ce59d6a40211fb4a2d33f835213c34
Ayodeji Jackson Oyeleye <emmanueljackie01@gmail.com>
2023-03-24 19:53:28 +0100

    fix: prox

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,8 @@ const app: Application = express();
 const PORT: string | number = process.env.PORT || 3000;
 const mongoString = process.env.DATABASE_URL;
 
+app.set('trust proxy', 1); 
+
 app.use(express.json());
 app.use(session(sessionOptions));
 app.use(cors(corsOptions));
