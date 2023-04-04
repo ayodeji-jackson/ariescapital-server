856d9e06a5e1f3aac4570ef0ac1dcba59aecbade
Ayodeji Jackson Oyeleye <emmanueljackie01@gmail.com>
2023-04-04 21:19:47 +0100

    feat: more auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -9,29 +9,36 @@ const router = Router();
 router.route('/auth/register').post(validate(UserSchema), async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = await UserModel.create({ ...req.body, password: bcrypt.hashSync(req.body.password, 10) });
-    res.status(201).json({ id: user.id });
+    res.status(201).json({ id: user.id, role: user.role });
   } catch (err) {
     return next(err); 
   }
 });
+
+router.route('/auth/login').get(async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session.userId) return res.status(403).json({ error: "you're not logged in" });
+  try {
+    if (await UserModel.findById(req.session.userId)) 
+      return res.json({ id: req.session.userId, role: req.session.userRole }); 
+  } catch (err) {
+    return next(err); 
+  }
+}); 
 
 router.route('/auth/login').post(validate(UserLoginSchema), async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body; 
     
     const user = await UserModel.findOne({ email });
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      res.status(400).json({ error: "invalid credentials" });
-      return;
-    }
+    if (!user || !bcrypt.compareSync(password, user.password)) 
+      return res.status(400).json({ error: "invalid credentials" });
 
-    if (req.session.userId && req.session.userId === user.id) {
-      res.status(200).json({ id: user.id });
-      return;
-    }
+    if (req.session.userId && req.session.userId === user.id) 
+      return res.status(200).json({ id: user.id, role: user.role });
 
-    req.session.userId = user.id;
-    res.status(200).json({ id: user.id });
+    req.session.userId = user.id; 
+    req.session.userRole = user.role; 
+    res.status(200).json({ id: user.id, role: user.role });
   } catch (err) {
     return next(err);
   }
