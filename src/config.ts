6af9fa0e666f3a314383228bd1b2292a8156a6f0
Ayodeji Jackson Oyeleye <emmanueljackie01@gmail.com>
2023-04-05 12:09:10 +0100

    fix: cookie config

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,11 @@ export const sessionOptions: SessionOptions = {
   secret: process.env.SESSION_SECRET!,
   resave: false,
   saveUninitialized: false, 
-  cookie: { maxAge: 1000 * 60 * 60 * 24 }, 
+  cookie: { 
+    maxAge: 1000 * 60 * 60 * 24, 
+    sameSite: `${process.env.NODE_ENV === "development" ? "lax" : "none"}`,
+    secure: process.env.NODE_ENV === "development" ? false : true, 
+  }, 
   store: MongoStore.create({
     mongoUrl: process.env.DATABASE_URL
   })
